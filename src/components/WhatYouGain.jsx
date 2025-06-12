228e6127622d581c0307537a5d03ff837288148e
OluwapelumeAA <abimbolapelumi370@gmail.com>
2025-06-12 22:47:08 +0100

    added learn in-demand skills section

--- src/components/WhatYouGain.jsx
+++ src/components/WhatYouGain.jsx
@@ -1,14 +1,17 @@
-import React from "react";
-// import { BiCertificate } from "react-icons/bi";
-// import { FiTool } from "react-icons/fi";
-// import { FaChalkboardTeacher } from "react-icons/fa";
-// import { MdRocketLaunch } from "react-icons/md";
-
+import { useState } from "react";
+import image from "../assets/images/dashboard.png";
 import "../styles/WhatYouGain.css";
 
 const WhatYouGain = () => {
+  const [perksOpen, setPerksOpen] = useState(false);
+
+  const toggleAccordion = () => setPerksOpen(!perksOpen);
+
+
+
   return (
     <section className="whatYouWillGainSection">
+      {/* End of What you stand to gain*/}
       <div className="firstContainer">
         <div className="textGrid">
           <h2 className="titleText">
@@ -24,7 +27,6 @@ const WhatYouGain = () => {
 
         <div className="features-grid">
           <div className="feature-box">
-            {/* <BiCertificate className="feature-icon" /> */}
             <h3>Earn a certificate</h3>
             <p>
               All Terra Learning finalists will be awarded a paid certificate of
@@ -58,6 +60,61 @@ const WhatYouGain = () => {
           </div>
         </div>
       </div>
+      {/* End of What you stand to gain*/}
+
+      {/* Start Learn in-demand skills seamlessly Section */}
+      <div className="secondContainer">
+        <div className="card1">
+          <img src={image} alt="Dashboard" className="cardImage" />
+        </div>
+
+        <div className="card2">
+          <div className="cardText">
+            <h2 className="titleText">
+              <span className="green">Learn</span> in-<br></br>demand skills
+              seamlessly
+            </h2>
+            <p className="textDescription">
+              Imagine learning in an environment built to give you the best
+              learning experience.
+            </p>
+            <p className="textDescription">
+              A blend of self-paced and instructor-led training with lots of
+              hands-on experience to ensure you are well-rounded in the track of
+              your choice.
+            </p>
+
+            {/* Start Perks Accordion Section */}
+             <div className="perksAccordion">
+              <div
+                className={`accordionHeader ${perksOpen ? "active" : ""}`}
+                onClick={toggleAccordion}
+              >
+                <span style={{ color: perksOpen ? "green" : "#2f2f2f" }}>Perks</span>
+                <span className="icon" style={{ color: perksOpen ? "green" : "#2f2f2f" }}>
+                  {perksOpen ? "−" : "+"}
+                </span>
+              </div>
+              {perksOpen && (
+                <ul className={`accordionContent ${perksOpen ? "show" : ""}`}>
+                  <li>
+                    Earn a certificate of completion at the end of the
+                    Internship
+                  </li>
+                  <li>Build your portfolio in the Terra Learning bootcamp</li>
+                  <li>
+                    Take part in team projects and create world-class projects
+                  </li>
+                </ul>
+              )}
+            </div>
+            {/* End Perks Accordion Section */}
+
+            <button className="signUp">Sign up for FREE</button>
+          </div>
+        </div>
+      </div>
+      {/* End Learn in-demand skills seamlessly Section */}
     </section>
   );
 };
